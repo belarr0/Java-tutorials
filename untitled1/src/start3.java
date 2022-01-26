7836a264cf13162d9cc72d13c97a4c963b990508
Bohdan Demych <belarr00ss@gmail.com>
2022-01-26 14:23:40 +0200

    paposhka s kodom

---
+++ untitled1/src/start3.java
@@ -0,0 +1,15 @@
+public class start3 {
+    public static void main (String [] args){
+        byte num = 100; //8 bit information (-128 + 127)
+        short num1 = 131; // 16 bit (-32 768 + 32 767)
+        int num2 = 141; // 32 bit (-2 147 483 648 + 2 147 483 647)
+        long num3 = 123; // 64 bit (-9 223 372 836 854 775 868 + 9 223 372 836 854 775 867)
+
+        float num4 = 1.1f; // 32 bit    6.7 after point
+        double num5 = 1.11d; // 64 bit  15 after point
+
+        char sym = 'A'; // or '3' (юникод!!) 16 bit
+
+        boolean isTrue = false; // 1 bit true;
+    }
+}
