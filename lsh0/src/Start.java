6415ca15cc9496130b639dccc01d5c32e021d0f1
Bohdan Demych <belarr00ss@gmail.com>
2022-01-26 15:16:36 +0200

    add new folder 'lsh0'

---
+++ lsh0/src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+
+    public final int NumberBuilding = 141; //final - константа; public/private - закрытая и открытая переменная которую можно/нельзя использовать в других проектах;
+
+    public static void main (String[] args){
+        int a = 5;
+        System.out.println("Hello world!");
+        System.out.printf("%d", a);
+    }
+}
