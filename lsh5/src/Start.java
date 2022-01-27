fc7655bac28084b20e2cf3541d59b4b82ac36533
Bohdan Demych <belarr00ss@gmail.com>
2022-01-27 09:47:41 +0200

    lesson 5

---
+++ lsh5/src/Start.java
@@ -0,0 +1,56 @@
+public class Start {
+    public static void main (String[] args){
+        /*
+        int num1 = 3;
+        int num2 = 4;
+
+        int result = num1 + num2;
+
+        System.out.println("Результат: " + result);
+         */
+
+        /*
+        int num1 = 7;
+        int num2 = 3;
+
+        int result = num1 - num2;
+        System.out.println("Результат: " + result);
+         */
+
+        /*
+        int num1 = 7;
+        int num2 = 3;
+
+        int result = num1 * num2;
+        System.out.println("Результат: " + result);
+         */
+
+        /*
+        int num1 = 20;
+        int num2 = 5;
+
+        int result = num1 / num2;
+        System.out.println("Результат: " + result);
+         */
+
+        /*
+        int num1 = 7;
+        int num2 = 3;
+
+        int result = num1 % num2;
+        System.out.println("Результат: " + result);
+         */
+
+        int n1 = 7;
+        int n2 = 6;
+        int n3 = 7;
+        int n4 = 5;
+
+        if (n1 == n2 || n3 > n4){ // && - 'и'; || - 'или';
+            System.out.println("Результат равен");
+        }
+        else{
+            System.out.println("Результат не равен");
+        }
+    }
+}
