07d55ece6dd4cab2770b281d499b74f6b8e64c1c
Bohdan Demych <belarr00ss@gmail.com>
2022-03-03 18:46:31 +0200

    add new lesson

--- lsh7/src/start.java
+++ lsh7/src/start.java
@@ -1,7 +1,35 @@
+import java.io.IOException; //System.in.read();
+
 public class start {
 
     public static void main (String[] args){
 
+        /*
+    for (int i = 0; i < 5;i++){
+        System.out.println("Итерация = " + i);
+    }
+     */
 
+        int count =0;
+
+        /*
+        while (count < 10){
+            System.out.println(count);
+            count++;
+            }
+         */
+
+        /*
+        do {
+            System.out.println(count);
+            count++;
+        }while (count < 10);
+         */
+
+        while (count < 10){
+            count++;
+            if ((count%2) != 0) continue;   //continue - возвращает в начало цикла. Также существует 'break' он прекращает цикл.
+                System.out.println(count);
+        }
     }
 }
---
+++ x_y/src/start.java
@@ -0,0 +1,10 @@
+public class start {
+
+    public static void main (String[] args){
+
+        int x = 7;
+        int y = 4;
+
+        System.out.print("x=" + x + "; y=" + y);
+    }
+}
