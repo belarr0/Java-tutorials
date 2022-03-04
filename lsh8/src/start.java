5fed43671261bc3e58626a7c56af69593cc0538a
Bohdan Demych <belarr00ss@gmail.com>
2022-03-04 12:33:01 +0200

    add new task lsh8

---
+++ lsh8/src/start.java
@@ -0,0 +1,32 @@
+public class start {
+    public static void main(String[] args){
+        /*
+        int[] nums = {1, 1, 1, 1};
+        ////////////[0][1][2][3]
+
+
+        int[] nums = new int[4];
+        ////////////[0][1][2][3]
+
+        for (int i = 0;i<4;i++){
+            nums[i] = i;
+        }
+
+        nums[3] = 100;
+
+        for (int e: nums){      //запоняет одномерный массив (укороченная версия)
+            System.out.println(e);
+        }
+
+
+        for(int i = 0;i<4;i++){
+            System.out.println("Элемент: [" + i + "]" + " " + nums[i]);
+        }
+         */
+
+        int table[][] = new int[3][4];
+        table[0] = new int[4];
+        table[1] = new int[4];
+        table[2] = new int[40];
+    }
+}
